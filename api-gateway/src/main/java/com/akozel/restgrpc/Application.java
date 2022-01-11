be24b560f74c8f6f5a05daf516210edf5243c9ef
Andrey Kozel <andrey.kozel@kaseya.com>
2022-01-11 22:49:01 +0300

    Setup micronaut projects

---
+++ api-gateway/src/main/java/com/akozel/restgrpc/Application.java
@@ -0,0 +1,11 @@
+package com.akozel.restgrpc;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+  public static void main(final String[] args) {
+    Micronaut.run(Application.class);
+  }
+
+}
